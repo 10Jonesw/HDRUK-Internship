633989fb67717104c620f8ac9b79bba560b3ba48
Wendy Jones <wendyjones@Wendys-MacBook-Air.local>
2021-07-06 13:20:22 +0100

    Commit 7

--- Script/SCRIPT_Owid Dataset.R
+++ Script/SCRIPT_Owid Dataset.R
@@ -67,7 +67,7 @@ options( scipen = 999 )
 ggplot() +
   geom_bar(aes(x = reorder(CurrentData_CountryName, -CurrentData_TotalCases), y = CurrentData_TotalCases), stat = "identity", fill = " Red") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-  ggtitle("Total number of COVID Cases per country (20-06-2021)") +
+  ggtitle("Total number of COVID Cases per country (30-06-2021)") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlab('') + ylab('Total Number of Cases') +
   scale_y_continuous(breaks=seq(0, 30000000, 5000000))
@@ -83,7 +83,7 @@ ggplot() +
   coord_cartesian(clip = "off") +
   theme(axis.text.y = element_text(hjust = 0.3, vjust = 0.5)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-  ggtitle("Total number of COVID Cases per country (20-06-2021)") +
+  ggtitle("Total number of COVID Cases per country (30-06-2021)") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlab('') + ylab('Total Number of Cases')
 
@@ -104,7 +104,7 @@ options( scipen = 999 )
 ggplot() +
   geom_bar(aes(x = reorder(CurrentData_CountryName, -CurrentData_TotalDeath), y = CurrentData_TotalDeath), stat = "identity", fill = " blue") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-  ggtitle("Total number of COVID deaths per country (20-06-2021)") +
+  ggtitle("Total number of COVID deaths per country (30-06-2021)") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlab('') + ylab('Total Number of Deaths') +
   scale_y_continuous(breaks=seq(0, 600000, 50000))
@@ -120,7 +120,7 @@ ggplot() +
   coord_cartesian(clip = "off") +
   theme(axis.text.y = element_text(hjust = 0.3, vjust = 0.5)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-  ggtitle("Total number of COVID deaths per country (20-06-2021)") +
+  ggtitle("Total number of COVID deaths per country (30-06-2021)") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlab('') + ylab('Total Number of Deaths')
 
@@ -129,7 +129,29 @@ ggsave("TotalDeathsLOG.png",width = 25, limitsize = FALSE, path = Graphs.dir)
 
 
 
+### CONTINENT LEVEL DATA ###
 
 
+# Look at how many continents there are 
+Continent <- unique(CurrentData[,2])
+
+# Subset dataset for Europe 
+Europe <- subset(CurrentData,CurrentData$continent=="Europe" )  
+
+# Genrate scatter graph for COVID deaths and cases 
+ggplot(Europe, aes(total_deaths_per_million, total_cases_per_million, color = human_development_index)) +
+  geom_point() +
+  geom_text(aes(label=location), size=3, hjust = 0.5, vjust = 1.5) +
+  geom_point(size = 2) +
+  scale_color_gradient(low="orange", high="Red") +
+  xlab('Total Number of Deaths per Million') + ylab('Total Number of Cases per Million') +
+  scale_y_continuous(breaks=seq(0, 200000, 20000)) + 
+  scale_x_continuous(breaks=seq(0, 4000, 500)) +
+  ggtitle("Total Number of COVID deaths and cases in Europe (30-06-2021)") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(colour = "Human Devlopment Index") +
+
+# Save Plot
+ggsave("Europe Cases and Deaths.png",width = 20, limitsize = FALSE, path = Graphs.dir)
 
   
